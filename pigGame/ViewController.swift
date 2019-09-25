fbbe5d874bc12a27386338340a2c19de0c2e5e00
ojas <ojasgupta25@icloud.com>
2019-09-25 10:06:30 -0500

    made classes for players

--- pigGame/ViewController.swift
+++ pigGame/ViewController.swift
@@ -12,19 +12,53 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    // making object of player one
+     var playone = player1()
+    // making player 2
+    var playtwo = player2()
+    
+    
+    
     
     @IBAction func playerTop(_ sender: UIButton) {
     }
     
     @IBAction func bottomPlayer(_ sender: Any) {
     }
-
+    
+    
 
     @IBOutlet weak var diceImage: UIImageView!
     
     @IBOutlet weak var topscore: UILabel!
     @IBOutlet weak var bottomscore: UILabel!
 }
+
+
+
+class player1: ViewController {  // player one object
+    var score = 0               // player one score
+    func add(_ s: Int)             // player one increasing score
+    {
+        score += s
+    }
+    
+    
+    
+}
+class player2 {  // player two object
+    var score2 = 0               // player two score
+    func add(_ s: Int)             // player two increasing score
+    {
+        score2 += s
+    }
+    
+}
+
 
